9629a030bb41e268fb7b810aaf19b393d7c8f2e0
seanmx96 <seanmx96@users.noreply.github.com>
2020-04-10 10:31:51 -0500

    Answer self-study questions. Create function to execute a GET request and build the trending topics section.

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,26 @@
 //
 //  Each tab should look like this:
 //    <div class="tab">topic here</div>
+
+const entryPoint = document.querySelector('div.topics')
+
+function get() {
+    axios.get('https://lambda-times-backend.herokuapp.com/topics')
+        .then( response => {
+            console.log('Success!')
+            const topics = response.data.topics
+            console.log(topics)
+
+            topics.forEach( topic => {
+                const tab = document.createElement('div')
+                tab.classList.add('tab')
+                tab.textContent = topic
+                entryPoint.appendChild(tab)
+            })
+        })
+        .catch( err => {
+            console.log(`There was an error: ${err}`)
+        })
+}
+
+get()
