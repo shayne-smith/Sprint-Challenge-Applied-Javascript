a570423f54d4b490b52684e44c35387b7a454f17
seanmx96 <seanmx96@users.noreply.github.com>
2020-04-10 10:12:57 -0500

    Created component for header using static HTML as data

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,32 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+function Header() {
+
+    // instantiate elements
+    const header = document.createElement('div')
+    const date = document.createElement('span')
+    const heading = document.createElement('h1')
+    const temp = document.createElement('span')
+
+    // structure elements
+    header.appendChild(date)
+    header.appendChild(heading)
+    header.appendChild(temp)
+
+    // add class names
+    header.classList.add('header')
+    date.classList.add('date')
+    temp.classList.add('temp')
+
+    // set text content
+    date.textContent = 'MARCH 28, 2019'
+    heading.textContent = 'Lambda Times'
+    temp.textContent = '98°'
+
+    return header
+}
+
+const headerContainer = document.querySelector('div.header-container')
+const headerStructure = Header()
+headerContainer.appendChild(headerStructure)
